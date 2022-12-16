216dd994bac9701b7899e0959dc0c225314d45ae
EdgarElizondo <edgar_7_23@hotmail.com>
2022-12-16 11:24:15 -0600

    Se agrego la carpeta 02_Arrays

--- 00_Recursividad/DecimalToBinary.py
+++ 00_Recursividad/DecimalToBinary.py
@@ -7,27 +7,4 @@ def Dec2Bin(n):
         return n%2 + 10*Dec2Bin(n//2)
          
 
-def stringifyNumbers(obj):
-    # TODO
-    lst = {}
-    for ind in obj:
-        print(type(obj[ind]))
-        if (type(obj[ind]) is dict)|(type(obj[ind]) is list):
-            lst[ind] = stringifyNumbers(obj[ind])
-        elif type(obj[ind]) is int:
-            lst[ind] = str(obj[ind])
-        else:
-            lst[ind] = obj[ind]
-    return lst
 
-obj = { "num": 1,
-        "test": [],
-        "data": {"val": 4,
-                "info": {"isRight": True,
-                        "random": 66
-                        }
-                }
-        }
-print(stringifyNumbers(obj))
-
-print(type(obj['num']))
