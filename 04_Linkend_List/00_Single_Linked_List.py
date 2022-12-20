dc125d009a31e5219a24ef13a3dca448e7ab1a09
EdgarElizondo <edgar_7_23@hotmail.com>
2022-12-20 16:03:18 -0600

    Class -Singly Linked List- creada en carpeta -Linked List-

---
+++ 04_Linkend_List/00_Single_Linked_List.py
@@ -0,0 +1,115 @@
+
+class node:
+    def __init__(self,value = None):
+        self.value = value
+        self.next = None
+
+class sl_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.elem = 0
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    #
+    def insert(self,value,location = 0):
+        if location > self.elem:
+            location = self.elem
+            print("Location is adjusted to list bounderies")
+        
+        newNode  = node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if (location == 0):
+                newNode.next = self.head
+                self.head = newNode    
+            elif (location == self.elem)|(location == -1):
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                index = 0
+                tempNode = self.head
+                while index != location - 1:
+                    tempNode = tempNode.next
+                    index +=1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+                if tempNode == self.tail:
+                    self.tail = newNode
+        self.elem += 1
+    # Trasverse Singly Linked List
+    def traverse(self):
+        if self.head is None:
+            print("The SLL is empty")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+    # Search index value
+    def index(self,nodeValue):
+        if self.head is None:
+            print("The SLL is empty")
+        else:
+            ind = 0
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return ind
+                ind += 1 
+                node = node.next
+            return "The value does not exist in this list"
+    #
+    def delete(self,location = 0):
+        if self.head is None:
+            print("The SLL is empty")
+        else:
+            if self.elem == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                if (location == 0):
+                    self.head = self.head.next
+                elif (location == self.elem)|(location == -1):
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+                else:
+                    index = 0
+                    tempNode = self.head
+                    while index < location -1:
+                        tempNode = tempNode.next
+                        index +=1
+                    nextNode = tempNode.next
+                    tempNode.next = nextNode.next
+
+    def remove(self,value):
+        if self.head is None:
+            print("The SLL is empty")
+        else:
+            pass
+
+
+single_list = sl_list()
+single_list.insert(1,1)
+single_list.insert(3,1)
+single_list.insert(7,1)
+single_list.insert(11,1)
+
+#single_list.__insert__(0,2)
+
+print([node.value for node in single_list])
+single_list.delete(-1)
+print([node.value for node in single_list])
